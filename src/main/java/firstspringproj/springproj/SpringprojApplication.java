51d4504009068d45c3c4ae6f98849c58b04e8c24
karts001 <karts001@gmail.com>
2022-12-23 10:26:49 +0000

    Added springboot project files

---
+++ src/main/java/firstspringproj/springproj/SpringprojApplication.java
@@ -0,0 +1,20 @@
+package firstspringproj.springproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+}
