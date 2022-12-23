3295f7e6f0babfb11231d4fa71ea6856ab430d86
karts001 <karts001@gmail.com>
2022-12-23 12:02:58 +0000

    Added Student class and landing page

--- src/main/java/firstspringproj/springproj/SpringprojApplication.java
+++ src/main/java/firstspringproj/springproj/SpringprojApplication.java
@@ -1,9 +1,16 @@
 package firstspringproj.springproj;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import firstspringproj.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +21,15 @@ public class SpringprojApplication {
 	}
 
 	@GetMapping("/")
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(
+			new Student(
+				1L,
+				"Shiva",
+				LocalDate.of(1996, Month.MARCH, 24),
+				"shiva@gmail.com",
+				27
+			)
+		);
 	}
 }
