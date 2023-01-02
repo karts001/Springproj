929873c20ed2b0dea593c537b2fb83ab359493ae
karts001 <karts001@gmail.com>
2023-01-02 21:01:51 +0000

    Can delete students from database by id

--- src/main/java/firstspringproj/springproj/student/StudentService.java
+++ src/main/java/firstspringproj/springproj/student/StudentService.java
@@ -1,6 +1,7 @@
 package firstspringproj.springproj.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,21 @@ public class StudentService {
 	}
 
     public void addNewStudent(Student student) {
-		System.out.println(student);
+		Optional<Student> StudentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+		if (StudentOptional.isPresent()){
+			throw new IllegalStateException("Email already being used");
+		};
+
+		studentRepository.save(student);
     }
+
+	public void deleteStudent(Long studentId) {
+		boolean exists = studentRepository.existsById(studentId);
+		if (!exists) {
+			throw new IllegalStateException("Student with id " + studentId + " does not exist");
+		}
+		studentRepository.deleteById(studentId);
+	}
+	
 }
